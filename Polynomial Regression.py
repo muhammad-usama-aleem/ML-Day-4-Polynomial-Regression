624e0de02b9f4bd05386b6271834aaa538025d5d
Muhammmad Usama Aleem <56190389+muhammad-usama-aleem@users.noreply.github.com>
2021-08-06 16:50:26 +0500

    inital

---
+++ Polynomial Regression.py
@@ -0,0 +1,18 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+print(dataset)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
